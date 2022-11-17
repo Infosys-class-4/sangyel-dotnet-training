b8c1fc694e0d111c29435ca2fb645ee3a41a965a
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-11-17 00:15:03 -0500

    Assignment solutions

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -104,14 +104,3 @@ for(int i = 1; i <=100; i++)
 }
 Console.WriteLine(" ");
 
-int l=0;
-
-for(int j = 1; j <= 3; j++)
-{
-    for(int k = 1; k <= 4; k++)
-    {
-        l= l+1;
-        Console.Write(l+" ");
-    }
-    Console.WriteLine(" ");
-}
