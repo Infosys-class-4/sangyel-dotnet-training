9e685d8d7f86e448c995e3a9c243ca2e999086d0
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-11-17 00:08:57 -0500

    loops

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -20,22 +20,70 @@ else
 Console.Write("Enter a name: ");
 string name = Console.ReadLine();
 string UpperCaseName = name.ToUpper();
-Console.Write(name + " Converted to Upper Case is: " + UpperCaseName);
+Console.WriteLine(name + " Converted to Upper Case is: " + UpperCaseName);
 
+int marks = 45;
+if(marks < 40)
+{
+    Console.WriteLine("failed");
+}
+else if(marks < 60)
+{
+    Console.WriteLine("Passed. You are placed in second division");
+}
+else if(marks < 80)
+{
+    Console.WriteLine("Passed. You are placed in first division");
+}
+else
+{
+    Console.WriteLine("Congratulations you have Passed with Distinction");
+}
 
+// Console.Write("Enter a number upto 5: ");
+// string romanNumerical = Console.ReadLine();
+// short number1 = short.Parse(romanNumerical);
+// if(number1 == 1)
+// {
+//     Console.WriteLine("1 roman numerical format is I");
+// }
+// else if(number1 == 2)
+// {
+//     Console.WriteLine("2 roman numerical format is II");
+// }
+// else if(number1 == 3)
+// {
+//     Console.WriteLine("3 roman numerical format is III");
+// }
+// else if(number1 == 4)
+// {
+//     Console.WriteLine("4 roman numerical format is IV");
+// }
+// else if(number1 == 5)
+// {
+//     Console.WriteLine("5 roman numerical format is V");
+// }
+// else
+// {
+//     Console.WriteLine("Invalid Input, please enter again");
+// }
 
+//switch - branching statement and selection statement
 
-
-
-
-
-
-
-
-
-
-
-
+// switch(n)
+// {
+//     case 1: result = "I";
+//     break;
+//     case 2: result = "II";
+//     break;
+//     case 3: result = "III";
+//     break;
+//     case 4: result = "IV";
+//     break;
+//     case 5: result = "V";
+//     break;
+// }
+// Console.WriteLine(result);
 
 
 
@@ -47,3 +95,23 @@ Console.Write(name + " Converted to Upper Case is: " + UpperCaseName);
 // Console.WriteLine(message);
 
 
+for(int i = 1; i <=100; i++)
+{
+    if(i % 2 == 0)
+    {
+        Console.Write(i+" ");
+    }
+}
+Console.WriteLine(" ");
+
+int l=0;
+
+for(int j = 1; j <= 3; j++)
+{
+    for(int k = 1; k <= 4; k++)
+    {
+        l= l+1;
+        Console.Write(l+" ");
+    }
+    Console.WriteLine(" ");
+}
