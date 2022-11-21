d5d8f20beff488004ba4e8690b642a817795411e
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-11-20 23:51:43 -0500

    #6

---
+++ CSharpFundamentals/assignments.cs
@@ -0,0 +1,22 @@
+/*
+//#4
+int l=0;
+for(int j = 1; j <= 3; j++)
+{
+    for(int k = 1; k <= 4; k++)
+    {
+        l= l+1;
+        Console.Write(l+" ");
+    }
+    Console.WriteLine(" ");
+}
+*/
+
+int ii, jj;
+for(ii=1;ii<90;ii++)
+{
+    for(jj=1;jj<=10;jj++)
+    {
+        Console.WriteLine(ii + " * " + jj + " = " + ii*jj);
+    }
+}
