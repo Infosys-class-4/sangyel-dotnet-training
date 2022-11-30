c6405b82f7d1db8d161b6c582546020acc7a7dd0
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-11-30 00:43:13 -0500

    Collection

--- CSharpFundamentals/H-Collection.cs
+++ CSharpFundamentals/H-Collection.cs
@@ -17,6 +17,18 @@ class Collection
         List<Person> people = new List<Person>();
 
         // CW - Define list of 10 even numbers.
-        
+        var collection1 = new List<string>("bishnu", "334","false","34.67");
+        //stack and queue
+        Stack<string> plates = new Stack<string>(); // LIFO
+        plates.Push("Plate1");
+        plates.Push("Plate2");
+        plates.Push("Plate3");
+        plates.Pop();
+
+        Queue<string> plateLine = new Queue<string>(); // FIFO
+        plateLine.Enqueue("Plate1");
+        plateLine.Enqueue("Plate2");
+        plateLine.Enqueue("Plate3");
+        plateLine.Dequeue();
     }
 }
