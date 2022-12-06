a89815a3958704fdf2f3df464e64e1335aefb65d
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-12-06 00:34:12 -0500

    animal.query

--- CSharpFundamentals/ClassesAndObjects/Animal.cs
+++ CSharpFundamentals/ClassesAndObjects/Animal.cs
@@ -1,13 +1,38 @@
 namespace LivingThings;
-
 //Top-level statements - implicit Main() entry point
-
 // Conditionals - branching
-
 class Animal
 {
+    public Animal() //parameter constructor
+    {
+
+    }
+// Parameterized constructor
+    public Animal(string animalCategory, byte legs)
+    {
+        category = animalCategory;
+        numberOfLegs = legs;
+    }
+    public Animal(string animalCategory, byte legs, string sn, bool d)
+    {
+        category = animalCategory;
+        numberOfLegs = legs;
+        domestic = d;
+        scientificName = sn;
+    }
+
     string category;
     string scientificName;
     byte numberOfLegs;
     bool domestic;
+
+    void animalList()
+    {
+        string [] listOfAnimals = {"wolf","dog","cat","ant","whale","eagle","gorilla","bear","penguin","elephant","tiger","lion"};
+        
+    }
 }
+// Create list/array of animals using Animal class we wrote.
+// Query that collection to find following:
+// Get list of all domestic animals
+// Get list of animals which has more than 4 legs
