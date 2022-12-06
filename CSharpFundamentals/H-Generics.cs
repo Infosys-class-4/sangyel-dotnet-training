6e23e6b070d0d3070c34dc87d8f8f27f94b404c0
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-12-06 00:26:29 -0500

    generic

--- CSharpFundamentals/H-Generics.cs
+++ CSharpFundamentals/H-Generics.cs
@@ -1,15 +1,14 @@
-// class Generic
-// {
-//     T Add<T>(T z, Tb)
-//     {
-//         uint sdfasf;
-//         return 23434;
-//     }
+class Generic
+{
+    void Add<T>(T z, T b)
+    {
+        uint sdfasf;
+    }
 
 
-//      void Test()
-//      {
-//         Add<int>(34, 56);
-//         Add<float>(34.34f, 56.89f);
-//      }
-// }
+     void Test()
+     {
+        Add<int>(34, 56);
+        Add<float>(34.34f, 56.89f);
+     }
+}
