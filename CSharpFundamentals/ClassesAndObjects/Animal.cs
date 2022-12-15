b7a46f0c83b7e25d8d234dbf4b7786e743dd1b22
Sangyel Tenzing <sangyel.tenz@gmail.com>
2022-12-14 21:54:29 -0500

    animal

--- CSharpFundamentals/ClassesAndObjects/Animal.cs
+++ CSharpFundamentals/ClassesAndObjects/Animal.cs
@@ -26,13 +26,4 @@ class Animal
     byte numberOfLegs;
     bool domestic;
 
-    void animalList()
-    {
-        string [] listOfAnimals = {"wolf","dog","cat","ant","whale","eagle","gorilla","bear","penguin","elephant","tiger","lion"};
-        
-    }
-}
-// Create list/array of animals using Animal class we wrote.
-// Query that collection to find following:
-// Get list of all domestic animals
-// Get list of animals which has more than 4 legs
+    
